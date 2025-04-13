6455892e4c0d4f7a7d0b5f4ffec122b633e5d7ef
PrashantAnand03 <prashantanandcse2025@gmail.com>
2025-04-13 21:45:57 +0530

    Updated message for backend running in server.js

--- server.js
+++ server.js
@@ -53,6 +53,10 @@ io.on('connection', (socket) => {
     });
 });
 
+app.get('/', (req, res) => {
+    res.send('Backend is running');
+});
+
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
